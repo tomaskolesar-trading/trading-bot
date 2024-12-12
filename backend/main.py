97356a2cc14d6c7413c28ffdefc6bd72de7a7253
tomaskolesar-trading <tomas.kolesar@email.cz>
2024-12-12 21:17:33 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from ml_models import MLPredictor
-from trading_strategy import TradingStrategy
+from backend.ml_models import MLPredictor
+from backend.trading_strategy import TradingStrategy
 import yfinance as yf
 from datetime import datetime
 import logging
